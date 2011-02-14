a03e68f771521a919c300a654df45a0ec369fb5b
darrellp <darrellp@77c8e68a-5a24-fe49-9ba7-f2ba7a9035d1>
2011-02-14 20:45:13 +0000

    Put XML comments in

--- IPartialSolution.cs
+++ IPartialSolution.cs
@@ -2,31 +2,78 @@
 
 namespace PuzzleSolver
 {
-	/// <summary>
-	/// A partial solution is just what it sounds like - a partial solution to the problem at hand.  In
-	/// terms of a search tree, it represents a node in the search tree.
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>	
+	/// A partial solution is just what it sounds like - a partial solution to the problem at hand.
+	/// In terms of a search tree, it represents a node in the search tree. 
 	/// </summary>
+	///
+	/// <remarks>	Darrellp, 2/14/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	public interface IPartialSolution
 	{
-		// Is this solution identical to another one?
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Determine if this solution identical to another one. </summary>
+		///
+		/// <param name="ps">	The partial solution to be compared to this one. </param>
+		///
+		/// <returns>	true if they are identical. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		bool IdenticalTo(IPartialSolution ps);
 
-		// Get the potential extensions to this partial solution.  In terms of the search tree, these
-		// are representatives of the children of this node.  If the count of the returned list is zero
-		// then this is a leaf node.
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	
+		/// Get the potential extensions to this partial solution.  In terms of the search tree, these
+		/// are representatives of the children of this node.  If the count of the returned list is zero
+		/// then this is a leaf node. 
+		/// </summary>
+		///
+		/// <returns>	A list of applicable extensions. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		List<IExtension> GetIExtensions();
 
-		// Apply an extension to get a new child node
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Apply an extension to get a new child node. </summary>
+		///
+		/// <param name="ext">			The extension to be applied. </param>
+		/// <param name="fReturnClone">	If true, the returned partial solution will be a newly created
+		/// 							clone of this partial solution with the extension applied.  If
+		/// 							false, we simply apply the extension to this partial solution and
+		/// 							return "this". </param>
+		///
+		/// <returns>	A partial solution with the extension applied. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		IPartialSolution PsApply(IExtension ext, bool fReturnClone);
 
-		// Sometimes between the time we generate an extension and the time we try to apply it, information
-		// may arise to make that extension obsolete.  If so, ExtensionFailed can return false.
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	
+		/// Sometimes between the time we generate an extension and the time we try to apply it,
+		/// information may arise to make that extension obsolete.  If so, ExtensionFailed can return false. 
+		/// </summary>
+		///
+		/// <param name="ext">	The extension to be applied. </param>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		void ExtensionFailed(IExtension ext);
 
-		// If this represents a solution, say so!
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	If this represents a solution, say so! </summary>
+		///
+		/// <returns>	true if this partial solution is a solution to the problem at hand. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		bool FSolved();
 
-		// Make a copy of this solution
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Makes a deep copy of this object. </summary>
+		///
+		/// <returns>	A copy of this object. </returns>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		IPartialSolution Clone();
 	}
 }
