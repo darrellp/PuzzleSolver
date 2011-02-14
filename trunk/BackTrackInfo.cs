7e4b9fd16036886bc14b80600770a525ae5f3836
darrellp <darrellp@77c8e68a-5a24-fe49-9ba7-f2ba7a9035d1>
2011-02-14 19:35:12 +0000

    Commenting and a bit of refactoring

---
+++ trunk/BackTrackInfo.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace PuzzleSolver
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>	Values that represent the reason for a backtrack . </summary>
+	///
+	/// <remarks>	Darrellp, 2/14/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public enum BacktrackReason
+	{
+		InitialEntry,
+		ForcedChoice,
+		Guess,
+		ExpertSystemApplication,
+		LeafNode,
+		NoMoreMoves,
+		RuleDetectedImpossibility,
+		GoalReachedButMoreNeeded,
+		GoalCountReached,
+		DuplicateSolution
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>	Information about the backtracking process. </summary>
+	///
+	/// <remarks>	Darrellp, 2/14/2011. </remarks>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public class BackTrackInfo
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Gets the reason for the backtrack. </summary>
+		///
+		/// <value>	The back track reason. </value>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		public BacktrackReason BackTrackReason { get; private set; }
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>	Gets the list of reasons. This only applies if the BackTrackReason 
+		/// is ExpertSystemApplication</summary>
+		///
+		/// <value>	A List of reasons. </value>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		public List<ReasonRulePair> ReasonList { get; private set; }
+		public IExtension Extension { get; private set; }
+
+		public BackTrackInfo(BacktrackReason btReason, List<ReasonRulePair> lstrrp, IExtension ext)
+		{
+			ReasonList = lstrrp;
+			Extension = ext;
+			BackTrackReason = btReason;
+		}
+
+		public override string ToString()
+		{
+			return BackTrackReason + (Extension == null ? "" : Extension.ToString());
+		}
+	}
+}
