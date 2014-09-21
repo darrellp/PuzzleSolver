ec4be1e498896f8985ce0dce0bba5c468adda42a
darrellp <darrellp@77c8e68a-5a24-fe49-9ba7-f2ba7a9035d1>
2014-09-21 01:31:21 +0000

    Carries implemented into the possibles machinery.

--- trunk/PuzzleSolverTests/PossiblesTests.cs
+++ trunk/PuzzleSolverTests/PossiblesTests.cs
@@ -24,19 +24,28 @@ namespace PuzzleSolverTests
 			var possibleAdd1 = new Possibles("?", 1);		// Fixed at 0
 			var possibleAdd2 = new Possibles("?", 1);		// Fixed at 0
 			var possibleCarry = Possibles.Carry(3);			// 0 or 1
-			var sum = possibleAdd1.Add(possibleAdd2);
-			sum = possibleCarry.Add(sum);
+			bool fCarry0, fCarry1;
+			var sum = possibleAdd1.Add(possibleAdd2, out fCarry0, out fCarry1);
+			Assert.IsTrue(fCarry0);
+			Assert.IsFalse(fCarry1);
+			sum = possibleCarry.Add(sum, out fCarry0, out fCarry1);
+			Assert.IsTrue(fCarry0);
+			Assert.IsFalse(fCarry1);
 			possibleSum.Set(sum);
 			Assert.AreEqual(3, possibleSum.Values);
-			sum = Possibles.Add(0x155, 0x2);				// Add 1
+			sum = Possibles.Add(0x155, 0x2, out fCarry0, out fCarry1);				// Add 1 to evens
+			Assert.IsTrue(fCarry0);
+			Assert.IsFalse(fCarry1);
 			Assert.AreEqual(sum, 0x2AA);					// Should add to odds
-			sum = Possibles.Add(0x155, 0x2, true);			// Subtract 1
+			sum = Possibles.Add(0x155, 0x2, out fCarry0, out fCarry1, true);		// Subtract 1
+			Assert.IsTrue(fCarry0);
+			Assert.IsFalse(fCarry1);
 			Assert.AreEqual(sum, 0x2AA);					// Should add to odds
-			sum = Possibles.Add(0x155, 0x10);				// Add 4
+			sum = Possibles.Add(0x155, 0x10, out fCarry0, out fCarry1);				// Add 4
 			Assert.AreEqual(sum, 0x155);					// Adds to evens
-			sum = Possibles.Add(0x30C, 0x18);				// {2,3,8,9} + {3,4}
+			sum = Possibles.Add(0x30C, 0x18, out fCarry0, out fCarry1);				// {2,3,8,9} + {3,4}
 			Assert.AreEqual(sum, 0xEE);						// {1,2,3,5,6,7}
-			sum = Possibles.Add(0x18, 0x30C);				// {3,4} + {2,3,8,9}
+			sum = Possibles.Add(0x18, 0x30C, out fCarry0, out fCarry1);				// {3,4} + {2,3,8,9}
 			Assert.AreEqual(sum, 0xEE);						// {1,2,3,5,6,7}
 		}
 	}
